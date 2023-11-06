0d6c8bfe22041512a26af8139b718911918e1920
Viktor Szépe <viktor@szepe.net>
2023-11-06 10:34:46 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,9 @@
 run_test()
 {
     TEST_NAME="$1"
-    TEST_COMMAND="$(cat "${TEST_NAME}/command")"
-    TEST_OUTPUT="$(cat "${TEST_NAME}/output")"
+
     echo "Testing ${TEST_NAME} ..."
-    test "$(eval "${TEST_COMMAND}")" = "${TEST_OUTPUT}"
+    test "$(. "${TEST_NAME}/command")" = "$(cat "${TEST_NAME}/output")"
 }
 
 find . -mindepth 1 -maxdepth 1 -type d -not -path "./.*" | while read -r FILE; do
