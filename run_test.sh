0fc8af78411889181c1978dbff3df6040cb38f69
Viktor Szépe <viktor@szepe.net>
2023-11-06 10:21:04 +0000

    include stderr

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ run_test()
 
     echo "Testing ${TEST_NAME} ..."
     echo "DBG: $(. "${TEST_NAME}/command") = $(cat "${TEST_NAME}/output")"
-    test "$(. "${TEST_NAME}/command")" = "$(cat "${TEST_NAME}/output")"
+    test "$(. "${TEST_NAME}/command" 2>&1)" = "$(cat "${TEST_NAME}/output")"
 }
 
 find . -mindepth 1 -maxdepth 1 -type d -not -path "./.*" | while read -r FILE; do
