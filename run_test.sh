91e66b5c4b9485c7d0f43cc49fe00a9b8e538ee1
Viktor Szépe <viktor@szepe.net>
2023-11-06 10:31:04 +0000

    fix

--- run_test.sh
+++ run_test.sh
@@ -2,11 +2,12 @@ run_test()
 {
     TEST_NAME="$1"
 
+    echo
     echo "Testing ${TEST_NAME} ..."
     TEST_CURRENT="$(. "${TEST_NAME}/command" 2>&1)"
-    TEST_OUTPUT="$(cat "${TEST_NAME}/output")"
-    echo "DBG: '${TEST_CURRENT}' = '${TEST_OUTPUT}'"
-    test "${TEST_CURRENT}" = "${TEST_OUTPUT}"
+    TEST_EXPECTED="$(cat "${TEST_NAME}/output")"
+    echo "DBG: '${TEST_CURRENT}' = '${TEST_EXPECTED}'"
+    test "${TEST_CURRENT}" = "${TEST_EXPECTED}"
 }
 
 find . -mindepth 1 -maxdepth 1 -type d -not -path "./.*" | while read -r FILE; do
